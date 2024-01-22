721d9bcf90cf4a9cfc44598f47cdfcbedf2ddc8f
chiangwyz <1119027571@qq.com>
2024-01-22 23:10:48 +0800

    feature: update reguralization

--- scikit-learn/source/003正则化-岭回归.py
+++ scikit-learn/source/003正则化-岭回归.py
@@ -7,15 +7,18 @@ from sklearn.linear_model import Ridge
 # 从sklearn.metrics导入mean_squared_error，用于计算均方误差
 from sklearn.metrics import mean_squared_error
 
-train_size = 20
-test_size = 12
+train_size = 200
+test_size = 120
+
 train_X = np.random.uniform(low=0, high=1.2, size=train_size)
-test_X = np.random.uniform(low=0.1, high=1.3, size=test_size)
+test_X = np.random.uniform(low=0, high=1.2, size=test_size)
 train_y = np.sin(train_X * 2 * np.pi) + np.random.normal(0, 0.2, train_size)
 test_y = np.sin(test_X * 2 * np.pi) + np.random.normal(0, 0.2, test_size)
+
 poly = PolynomialFeatures(6)  # 次数为6
 train_poly_X = poly.fit_transform(train_X.reshape(train_size, 1))
 test_poly_X = poly.fit_transform(test_X.reshape(test_size, 1))
+
 model = Ridge(alpha=1.0)
 model.fit(train_poly_X, train_y)
 train_pred_y = model.predict(train_poly_X)
