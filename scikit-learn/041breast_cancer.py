88be84a0e05843f91b4fefc42255ad1e50a3b38d
chiangwyz <1119027571@qq.com>
2024-02-17 16:27:36 +0800

    feature: add breast cancer data

--- scikit-learn/041breast_cancer.py
+++ scikit-learn/041breast_cancer.py
@@ -15,24 +15,47 @@ y = 1 - data.target
 # 设置numpy打印选项，限制小数点后三位
 np.set_printoptions(precision=3, suppress=True)
 print(data.DESCR)
-print('%.2f', X[:5])
+# print('%.2f', X[:5])
 
 # 选择前10个特征
-X = X[:, :10]
+# X = X[:, :]
 
 # 划分训练数据和测试数据
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # 初始化逻辑回归模型
 model = LogisticRegression(max_iter=10000)
 
 # 训练模型
-model.fit(X_train, y_train)
+model.fit(X, y)
 
 # 在测试集上进行预测
-y_pred = model.predict(X_test)
+y_pred = model.predict(X)
 
 # 如果需要，可以评估模型性能
 # 例如，使用accuracy_score来计算准确率
-accuracy = accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y, y_pred)
 print('Accuracy: {}'.format(accuracy))
+
+from sklearn.metrics import precision_score
+
+precision = precision_score(y, y_pred)
+print('precision: {}'.format(precision))
+
+from sklearn.metrics import recall_score
+recall = recall_score(y, y_pred)
+print('recall: {}'.format(recall))
+
+from sklearn.metrics import f1_score
+f1 = f1_score(y, y_pred)
+print('f1: {}'.format(f1))
+
+proba = model.predict_proba(X)
+print("proba = ", proba)
+
+y_pred2 = (model.predict_proba(X)[:, 1]>0.1).astype(np.int)
+
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y, y_pred)
+print(cm)
--- scikit-learn/评估方法/breast-cancer_introduction.md
+++ scikit-learn/评估方法/breast-cancer_introduction.md
@@ -2,3 +2,24 @@ load_breast_cancer()函数是Scikit-learn库中的一个函数，用于加载乳
 
 # 基本信息
 
+* 目标：预测肿瘤是良性（benign）还是恶性（malignant）。
+
+* 数据集特点：数据集由569个样本组成，每个样本包含30个特征，这些特征是从数字化图像的细胞核中计算得出的。特征包括细胞核的大小、形状、纹理等方面的量化度量。
+
+* 标签：数据集中的目标变量是一个0-1变量，表示肿瘤的类型：良性（值为0）或恶性（值为1）。
+
+# 特征详细
+数据集中的特征是从图像中提取的细胞核特征，具体包括：
+
+* 半径（mean of distances from center to points on the perimeter）
+* 纹理（standard deviation of gray-scale values）
+* 周长
+* 面积
+* 平滑度（local variation in radius lengths）
+* 紧凑度（perimeter^2 / area - 1.0）
+* 凹度（severity of concave portions of the contour）
+* 凹点（number of concave portions of the contour）
+* 对称性
+* 分形维数（"coastline approximation" - 1）
+
+这些特征被计算为三个不同的度量：均值（mean）、标准误差（se）、最大值（worst），这些计算出的特征一起构成了数据集的30个特征。
